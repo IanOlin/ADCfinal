566eaa3f4d7185e491ba7a439058e959996550be
arianaolson419 <ariana.olson@students.olin.edu>
2017-12-07 12:12:52 -0500

    Generate noise

--- create_send_data.m
+++ create_send_data.m
@@ -1,9 +1,14 @@
-x = ones(100000, 1);
-x = x * .6;
+% x = ones(1000000, 1);
+% for i = 1:length(x)/500
+%     x((i - 1) * 500 + 1: (i - 1) * 500 + 250) = -1;
+% end
+
+
+% x = x * .6;
+
+x = wgn(10000000, 1, 1);
 write_usrp_data_file(x);
 
-% ./tx_samples_from_file --file
-% C:\Usets\aolson\Documents\MATLAB\AnalogDigitalcomm\Final\tx.dat --rate
-% 2500000 --freq 2497000000 --gain 20 --type float
+% ./tx_samples_from_file --file C:\Users\aolson\Documents\MATLAB\AnalogDigitalcomm\Final\tx.dat --rate 2500000 --freq 2497000000 --gain 20 --type float
 
 % C:\Program Files\UHD\lib\uhd\examples
