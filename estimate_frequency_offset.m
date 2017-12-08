36c996a9cb7171133e4ae684e7b5391e4ebf715c
arianaolson419 <ariana.olson@students.olin.edu>
2017-12-07 21:41:53 -0500

    Implement frequency offset estimation

---
+++ estimate_frequency_offset.m
@@ -0,0 +1,10 @@
+function f_est = estimate_frequency_offset(y)
+   % Estimate the frequency offset of a received signal.
+   % y: the received signal.
+   % f_est: the frequency offset estimate in radians.
+   Y1 = abs(fftshift(fft(y.^4)));
+   fs = linspace(-pi, pi, length(Y1));
+   plot(fs, Y1);
+   [~, I] = max(Y1);
+   f_est = fs(I(1)) / 4;
+end
