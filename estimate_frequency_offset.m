b354dad940e34a0648df6fdb1464efa86747ba37
arianaolson419 <ariana.olson@students.olin.edu>
2017-12-09 14:16:28 -0500

    Stuff from meeting

--- estimate_frequency_offset.m
+++ estimate_frequency_offset.m
@@ -4,7 +4,6 @@ function f_est = estimate_frequency_offset(y)
    % f_est: the frequency offset estimate in radians.
    Y1 = abs(fftshift(fft(y.^4)));
    fs = linspace(-pi, pi, length(Y1));
-   plot(fs, Y1);
    [~, I] = max(Y1);
    f_est = fs(I(1)) / 4;
    disp(f_est);
--- read_received.m
+++ read_received.m
@@ -5,7 +5,7 @@ fclose(f1);
 known = tmp(1:2:end)+1i*tmp(2:2:end);
 plot(tmp(2:2:end))
 
-f1 = fopen('rx1.dat', 'r');
+f1 = fopen('rx1209.dat', 'r');
 tmp = fread(f1,'float32');
 fclose(f1);
 rx = tmp(1:2:end)+1i*tmp(2:2:end);
@@ -18,8 +18,20 @@ close all;
 [~, I] = max(abs(corr));
 t = lags(I);
 
-figure; hold on;
-data_only = rx((t + 10 * (length(known) / 2)):end);
-subplot(2,1,1); plot(real(data_only));
-subplot(2,1,2); plot(imag(data_only));
-locked = costas_loop(data_only, .1);
+% figure; hold on;
+start_data = (t + 10 * (length(known) / 2));
+data_only = rx(start_data: start_data + 8e5);
+% subplot(2,1,1); plot(real(data_only));
+% subplot(2,1,2); plot(imag(data_only));
+
+Kp = 10;
+Ki = 15;
+Kd = 0;
+locked = phase_locked_loop(data_only, Kp, Ki, Kd);
+plot(locked, '*');
+
+threshold_real = (real(locked) > 0) - (real(locked) <= 0);
+threshold_imag = (imag(locked) > 0) - (imag(locked) <= 0);
+figure;
+plot(threshold_real + 1i * threshold_imag, '*');
+ 
