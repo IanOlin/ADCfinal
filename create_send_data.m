710bcdb3ef2db075d7d36c130b6ba6e6ddc42c41
IanOlin <ian.paul@students.olin.edu>
2017-12-09 13:42:50 -0500

    new tx.dat and script

--- create_send_data.m
+++ create_send_data.m
@@ -6,7 +6,16 @@
 
 % x = x * .6;
 
-x = wgn(10000000, 1, 1);
+x1 = wgn(10000, 1, 1);
+
+f1 = fopen('known.dat', 'r');
+x2 = fread(f1,'float32');
+fclose(f1);
+
+t = 0:1/2.5e6:3;
+x3 = .6*square(2*pi*1000*t);
+
+x = [x1;x2;x3.'];
 write_usrp_data_file(x);
 
 % ./tx_samples_from_file --file C:\Users\aolson\Documents\MATLAB\AnalogDigitalcomm\Final\tx.dat --rate 2500000 --freq 2497000000 --gain 20 --type float
